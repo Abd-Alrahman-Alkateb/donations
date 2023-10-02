b64b165015e28cbeabb871f8ff38236c5dd4a837
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2023-10-02 19:21:58 +0300

    Create donation resource and add some packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
+import {  GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 import {ApolloServerPluginLandingPageLocalDefault} from 'apollo-server-core';
+import { DonationsModule } from './donations/donations.module';
+import { GraphQLDateTime } from 'graphql-iso-date';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      driver: ApolloDriver,
       playground:false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
+      resolvers: {DateTime: GraphQLDateTime},
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
